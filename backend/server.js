ea9ca5aeb48118cce8c083586b53bc60cf285d42
0x01amar <amarnath82928@gmail.com>
2026-03-21 13:32:26 +0530

    fix update product button

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,8 @@ if (!fs.existsSync(envPath)) {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const serveGridFSImage = async (req, res) => {
   const bucket = getGridFSBucket();
 
